fe44303b087379b041180e07a1a2b22c8f1c869e
Thorsten Mueller <tm@thorsten-voice.de>
2024-02-09 17:16:12 +0100

    Draft script to save wavefile from PiperTTS http server process.

---
+++ TTS/PiperTTS/PiperTTS_Server2WaveFile.py
@@ -0,0 +1,13 @@
+import requests
+
+textToSpeak = "This is a text to be spoken using the locally running Piper TTS server process."
+urlPiper = "http://localhost:5000"
+outputFilename = "output.wav"
+∏
+payload = {'text': textToSpeak}
+
+r = requests.get(urlPiper,params=payload)
+
+with open(outputFilename, 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=128):
+        fd.write(chunk)
